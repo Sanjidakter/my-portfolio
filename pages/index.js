28308fd6ad0960a8bc6c39573fd7ac8aefbd65ad
Sanjida Akter <sanjidakter2304@gmail.com>
2024-08-09 19:57:18 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1g6f23_ajXVDR07EQiMUloJsadeoqidrs/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1d6VAHDzTXMFKN6_7josoLMaHFWiccZFM/view?usp=sharing"
                 >
                   Resume
                 </a>
